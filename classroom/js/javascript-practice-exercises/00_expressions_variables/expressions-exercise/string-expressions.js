9398e070918f7fc677be8693185c1addeae86a7f
369webforall <369webforall@gmail.com>
2024-01-22 15:21:18 +0545

    data types and operators

---
+++ classroom/js/javascript-practice-exercises/00_expressions_variables/expressions-exercise/string-expressions.js
@@ -0,0 +1,7 @@
+console.log('river' + 'town'); // 'rivertown'
+console.log('cat' + 'dog');
+console.log('New' + ' York'); // New York
+console.log("runner's knee" + '!'); // runner's knee!
+console.log('man ' + 'bear ' + 'pig');
+
+let i = undefined;
---
+++ classroom/js/javascript-practice-exercises/00_expressions_variables/first/script.js
@@ -0,0 +1,19 @@
+// Fundamenta of any programming language
+
+// Data type
+// number, string, boolean, null, undefined, array, object
+// number -  1, 2, 4, 5, 5.6,
+
+// string - "my name is dev and i am 45"
+
+// boolean - true / false
+
+// null - nothing
+
+// undefined - value before assigniment.
+
+// array - [45, 'dev', true, null, undefined, [1,2,3,4,5], {} ]
+
+//  i want list of subject in class 9, ['Math', 'science', 'history']
+
+// object = {id:1, name:'dev', age:45, teacher:'web developer', obj1:{}, arr:[]}
