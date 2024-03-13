95473bddfe99ed20dd46ae4b2df0229b869e5285
369webforall <369webforall@gmail.com>
2024-03-13 17:45:43 +0545

    try catch

--- 4-javascript/Promise/script.js
+++ 4-javascript/Promise/script.js
@@ -23,40 +23,102 @@
 // .then((data)=>console.log(data))
 // .catch((error)=>console.log(error))
 
-function walkDog() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("You walk the dog 🐕");
-    }, 1500);
-  });
+// Walk the dog
+// clean kitchen
+// take out the trash
+
+// function walkDog(callback){
+//     setTimeout(()=>{
+//         console.log("You walk the dog 🐕‍🦺");
+//         callback();
+//     },1500)
+// }
+
+// function cleanKitchen(callback){
+//     setTimeout(()=>{
+//         console.log("You clean the kitchen 🥝");
+//         callback()
+//     },3000)
+// }
+
+
+// function takeOutTrash(callback){
+//     setTimeout(()=>{
+//         console.log("You take out the trash 🚜");
+//         callback();
+//     },500)
+// }
+
+// // first use callback function.
+
+// walkDog(()=>{
+//     cleanKitchen(()=>{
+//         takeOutTrash(()=>console.log("all the task is completed"))
+//     })
+// })
+
+// Promise
+
+function walkDog(){
+    return new Promise((resolve, rejected)=>{
+        setTimeout(()=>{
+            resolve("You walk the dog 🐕‍🦺");   
+        },1500)
+    })
+    
 }
 
-function cleanKitchen() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("You clean kitchen 🔪");
-    }, 2500);
-  });
+function cleanKitchen(){
+    return new Promise((resolve, rejected)=>{
+    setTimeout(()=>{
+        resolve("You clean the kitchen 🥝");
+        
+    },3000)
+})
 }
 
-function takeOutTrash() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("You takeout trash 🚜");
-    }, 500);
-  });
+
+function takeOutTrash(){
+    return new Promise((resolve, rejected)=>{
+
+    setTimeout(()=>{
+        let trashTruck = false;
+        if(trashTruck){
+            resolve("You take out the trash 🚜");
+        }else {
+            rejected("No trash truck, u canot take out the tash")
+        }
+           
+    },500)
+})
 }
 
-async function workOrder(){
-const walkdog = await walkDog();
-console.log(walkdog);
+// walkDog().then((data)=>{
+//     console.log(data);
 
-const clean =await cleanKitchen();
-console.log(clean)
+//     return cleanKitchen();
+// }).then((data)=>{
+//     console.log(data);
+//     return takeOutTrash();
+// }).then((data)=>{
+// console.log(data);
+// console.log("All the task is completed")
+// }).catch((err)=>console.log(err))
 
-const trash = await takeOutTrash();
-console.log(trash)
-console.log("All the work is completed")
+async function workInOrder(){
+try {
+    const walkdog = await walkDog();
+    console.log(walkdog);
+    const cleanK = await cleanKitchen();
+    console.log(cleanK);
+    
+    const takeTrash = await takeOutTrash()
+    console.log(takeTrash)
+    
+    console.log("Hey the work is done in squence"); 
+} catch (error) {
+    console.log(error)
+}
 }
 
-workOrder();
+workInOrder();
