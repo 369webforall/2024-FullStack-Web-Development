aec5af06f4540d42558e55ef6204b28157cd9180
369webforall <369webforall@gmail.com>
2024-03-13 14:17:43 +0545

    promise basic

--- 4-javascript/Promise/script.js
+++ 4-javascript/Promise/script.js
@@ -1,2 +1,29 @@
+// promise is object = {}
+// status of promise = pending, fullfilled and rejected.
+// new Promise((resolve, reject)=>{})
+
+// let promise = new Promise((resolve, reject)=>{})
+
+// console.log(promise)
+
+// function driveCar(){
+//     return new Promise((resolve, reject)=>{
+//         let licence = false;
+//         if(licence){
+//             resolve("Your can drive")
+//         }else {
+//             reject("You canot drive")
+//         }
+//     })
+// }
+
+// let promise = driveCar()
+
+// driveCar()
+// .then((data)=>console.log(data))
+// .catch((error)=>console.log(error))
+
+
+
 
 
