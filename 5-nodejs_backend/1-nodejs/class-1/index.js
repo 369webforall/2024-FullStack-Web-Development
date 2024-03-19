bfb4ee8fb250167fe0a0e56511d8d21925459270
369webforall <369webforall@gmail.com>
2024-03-19 13:43:40 +0545

    express

--- 5-nodejs_backend/1-nodejs/class-1/index.js
+++ 5-nodejs_backend/1-nodejs/class-1/index.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const app = express();
 
-function calcualteSum(n){
-    let ans =0;
-    for(let i =1; i <= n; i++){
-        ans = ans + i
+function calculateSum(n){
+    let ans = 0;
+    for(let i =1; i<=n; i++){
+        ans = ans + i;
     }
-
     return ans;
-
 }
 
-const result = calcualteSum(20)
-console.log(result)
+app.get('/', (req, res) => {
+  const n = req.query.n;
+  const ans = calculateSum(n);
 
+  res.send(ans);
+});
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
-app.get('/doctor', (req, res)=>{
-    let n = req.query.n;
-    const ans = calcualteSum(n)
-  
-    res.status(200).send(ans.toString());
-})
-
-app.listen(3000, ()=>{
-    console.log("server is running in port 3000")
-})
 
