5e96007a4191d54f700dcfd5e4181ebb651c6bb6
369webforall <369webforall@gmail.com>
2024-03-20 13:17:35 +0545

    mongodb

--- 5-nodejs_backend/1-nodejs/class-2/controllers/userControllers.js
+++ 5-nodejs_backend/1-nodejs/class-2/controllers/userControllers.js
@@ -1,29 +1,60 @@
 
-exports.getUsers = (req, res) =>{
+
+// exports.getUsers = (req, res) =>{
+//     res.send("Get all the users")
+// }
+
+// // Get user by ID
+// exports.getUserById = (req, res) => {
+//     // Your logic to fetch a specific user based on ID
+//     res.send('GET request to a specific user with id');
+// };
+
+// // Create a new user
+// exports.createUser = (req, res) => {
+//     // Your logic to create a new user
+//     res.send('POST request to create a new user');
+// };
+
+// // Update a user
+// exports.updateUser = (req, res) => {
+//     // Your logic to update a user
+//     res.send('PUT request to update a user');
+// };
+
+// // Delete a user
+// exports.deleteUser = (req, res) => {
+//     // Your logic to delete a user
+//     res.send('DELETE request to delete a user');
+// };
+
+const getUsers = (req, res) =>{
     res.send("Get all the users")
 }
 
 // Get user by ID
-exports.getUserById = (req, res) => {
+const getUserById = (req, res) => {
     // Your logic to fetch a specific user based on ID
     res.send('GET request to a specific user with id');
 };
 
 // Create a new user
-exports.createUser = (req, res) => {
+const createUser = (req, res) => {
     // Your logic to create a new user
     res.send('POST request to create a new user');
 };
 
 // Update a user
-exports.updateUser = (req, res) => {
+const updateUser = (req, res) => {
     // Your logic to update a user
     res.send('PUT request to update a user');
 };
 
 // Delete a user
-exports.deleteUser = (req, res) => {
+const deleteUser = (req, res) => {
     // Your logic to delete a user
     res.send('DELETE request to delete a user');
 };
 
+
+module.exports = {getUsers, getUserById, createUser, updateUser, deleteUser}
